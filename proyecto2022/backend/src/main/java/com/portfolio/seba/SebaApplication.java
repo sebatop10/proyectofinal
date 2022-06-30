2b1df4135523889a3fb67d68738d53e4ebdcf075
sebatop10 <luissebastian_21@hotmail.com>
2022-06-30 03:37:44 -0300

    carpeta backend
    creando la carpeta backend en spring

---
+++ proyecto2022/backend/src/main/java/com/portfolio/seba/SebaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.seba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebaApplication.class, args);
+	}
+
+}
---
+++ proyecto2022/backend/src/test/java/com/portfolio/seba/SebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.seba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
